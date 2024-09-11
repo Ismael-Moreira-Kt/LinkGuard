a09e8d78d3ee466b28916affe6c059897785e00a
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-11 15:21:57 +0100

    feat: Implement check_link function to verify HTTP status.

--- src/main.py
+++ src/main.py
@@ -2,3 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import argparse
+
+
+def check_link(url):
+    try:
+        response = requests.get(url)
+        
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
